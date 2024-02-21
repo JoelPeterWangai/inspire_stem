0e2313d3a893f0522667005982e435e5ae30ce76
JoelPeterWAngai <mugocijoseph@gmail.com>
2024-02-21 09:37:18 +0300

    fifth commit

--- week_2/Quadratic.py
+++ week_2/Quadratic.py
@@ -2,14 +2,14 @@
 # Date : 20/02/2024
 # Name : Joel Peter
 
-a =float (input(" eneter coefficient of first term: "))
-b = float(input (" eneter coefficient of second term: "))
+a =float (input(" enter coefficient of first term: "))
+b = float(input (" enter coefficient of second term: "))
 c =float( input("enter constant: "))
 
 d = float((float(b)**2) - 4 * float(a) * float(c))
 
-x_1 =( (-b +  math.sqrt(d)) / 2 * a)
-x_2 =( (-b -  math.sqrt(d)) / 2 * a)
+x_1 =( (-b + math.sqrt(d)) / 2 * a)
+x_2 =( (-b - math.sqrt(d)) / 2 * a)
 
 print("The solution of the quadratic equation is :", x_1)
 print("The solution of the quadratic equation is :", x_2)
