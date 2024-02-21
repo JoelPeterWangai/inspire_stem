d6ada8f6f46baba30bb5f0631fecdefaf4b4a6df
JoelPeterWAngai <mugocijoseph@gmail.com>
2024-02-21 09:38:57 +0300

    fifth commit

--- week_2/Quadratic.py
+++ week_2/Quadratic.py
@@ -6,7 +6,7 @@ a =float (input(" enter coefficient of first term: "))
 b = float(input (" enter coefficient of second term: "))
 c =float( input("enter constant: "))
 
-d = float((float(b)**2) - 4 * float(a) * float(c))
+d = float(((b)**2) - 4 * (a) * (c))
 
 x_1 =( (-b + math.sqrt(d)) / 2 * a)
 x_2 =( (-b - math.sqrt(d)) / 2 * a)
